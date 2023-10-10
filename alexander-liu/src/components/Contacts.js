4e14b10b5c601289e119b955bf7e9f89304be032
AlexL374 <alexandernumber2@gmail.com>
2023-10-10 15:51:00 -0400

    Better for smaller viewports

--- alexander-liu/src/components/Contacts.js
+++ alexander-liu/src/components/Contacts.js
@@ -1,20 +1,35 @@
 import './css/Contacts.css';
 import DownloadIcon from './vector-sign-of-download-icon.jpg';
 import resume from './Alexander-Liu-Resume.pdf';
+import { useEffect, useState } from 'react';
+import { slide as Menu } from 'react-burger-menu'
 
 function Contacts() {
+
+    const [smallWindow, setSmallWindow] = useState(false);
+
+    useEffect(() => {
+        if (window.innerWidth < 845) {
+            setSmallWindow(true);
+        }
+    }, []);
+
     return (
         <div class='outer'>
             <a class='name' href="#">alexander liu</a>
+            {smallWindow ? 
+                <div>
+
+                </div>
+            :
             <div class='right'>
                 <a class='link' href="#school">School</a>
                 <a class='link' href='#experiences'>Experiences</a>
                 <a class='link' href='#skills'>Skills</a>
                 <a class='link' href='#accolades'>Accolades</a>
-            {/*<p class='link'>About</p>*/}
-            <a class='link' href={resume} target='_blank'>Resume</a>
-            <a class='link' href="#footer">Contact</a>
-            </div>
+                <a class='link' href="#footer">Contact</a>
+                <a class='link' href={resume} target='_blank'>Resume</a>
+            </div>}
         </div>
     );
 }
--- alexander-liu/src/components/Footer.js
+++ alexander-liu/src/components/Footer.js
@@ -12,13 +12,13 @@ function Footer() {
         <div id="footer">
             <div class='footer-outer' id="footer">
                     <div class='footer-contact'>
-                        <a class='footer-contact-text'>
+                        <a class='footer-contact-text column1'>
                             (631)-353-8891
                         </a>
-                        <a class='footer-contact-text'>
+                        <a class='footer-contact-text column2'>
                             alexandernumber2@gmail.com
                         </a>
-                        <a class='footer-contact-text linked-in' href="https://www.linkedin.com/in/alexander-liu-al/" target='_blank'>
+                        <a class='footer-contact-text linked-in column3' href="https://www.linkedin.com/in/alexander-liu-al/" target='_blank'>
                             www.linkedin.com/in/alexander-liu-al/
                         </a>
                     </div>
